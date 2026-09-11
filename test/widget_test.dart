f732677c470af7074a53561419da9c6c0df4be8d
Jaisun <mjaisunreddy@gmail.com>
2026-09-12 00:15:51 +0530

    Add application tests

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+﻿import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('CONNECT App smoke test', (WidgetTester tester) async {
+    // Basic test stub to prevent compilation failures during development
+    expect(true, isTrue);
+  });
+}
